7ddffe0633d882bf783c6de11c236bd368a38f13
Christopher Towner <christopher.allen.towner@gmail.com>
2019-05-02 15:02:33 -0500

    mapped snow ids to tickets with customfield; update or create based on presence of id

--- s2j.py
+++ s2j.py
@@ -6,37 +6,67 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+#headers = {"Accept":"application/json", "Content-Type":"application/json"}
+
 # setup url for all incidents assigned to SNOW_USER
-snow_url = 'https://' + os.environ['SNOW_NAME'] + '.service-now.com/api/now/table/incident?active=true&assigned_to=' + os.environ['SNOW_USER']
+url = 'https://' + os.environ['SNOW_NAME'] + '.service-now.com/api/now/table/incident?assigned_to=' \
+        + os.environ['SNOW_USER'] + '&active=true'
 
 # get all incidents
-headers = {"Accept":"application/json"}
-snow_response = requests.get(snow_url, auth=(os.environ['SNOW_USER'],os.environ['SNOW_PASS']), headers=headers, verify=True)
+response = requests.get(url, auth=(os.environ['SNOW_USER'],os.environ['SNOW_PASS']), verify=True)
 
 # log err and exit if unable to get snow incidents
-if snow_response.status_code != 200:
+if response.status_code != 200:
     logger.fatal("Unable to get incidents from SNow")
     exit(1)
+incidents = json.loads(response.text)['result']
 
+# TODO set maxResults to disable pagination
 # setup jira query for all issues in project assigned to user and incomplete
-jira_url = 'https://' + os.environ['JIRA_NAME'] + '.atlassian.net/rest/api/latest/search?jql=project=' \
-        + os.environ['JIRA_PROJECT'] + '+and+assignee=' + os.environ['JIRA_ASSIGNEE'] + '+and+statusCategory!=done'
+url = 'https://' + os.environ['JIRA_NAME'] + '.atlassian.net/rest/api/latest/search?jql=project=' \
+        + os.environ['JIRA_PROJECT'] + '+and+labels=snow+and+statusCategory!=done'
 
 # get all issues
-jira_response = requests.get(jira_url, auth=(os.environ['JIRA_USER'],os.environ['JIRA_TOKEN']), headers=headers, verify=True)
+response = requests.get(url, auth=(os.environ['JIRA_USER'],os.environ['JIRA_TOKEN']), verify=True)
 
 # log err and exit if unable to get jira tickets
-if jira_response.status_code != 200:
+if response.status_code != 200:
     logger.fatal("Unable to get issues from JIRA")
     exit(1)
+issues = json.loads(response.text)['issues']
 
-issues = json.loads(jira_response.text)['issues']
+# jira customfield to store snow id
+customfield = os.environ['JIRA_FIELD']
+
+# create issue dictionary based on snow ids
+snowissues = {}
+logger.debug('found %i issue(s)', len(issues))
+for issue in issues:
+    snowissues['021fa4070f8533c01cfedb0be1050e37'] = issue
+    #snowissues[issue['fields'][customfield]] = issue
 
 # read-in each incident
-incidents = json.loads(snow_response.text)['result']
-logger.debug('found %i incidents', len(incidents))
+logger.debug('found %i incident(s)', len(incidents))
 for incident in incidents:
-    sys_id = incident['sys_id']
-    summary = incident['short_description']
-    description = incident['description']
-    logger.debug('(%s, %s)', sys_id, summary);
+    if incident['sys_id'] in snowissues:
+        logger.debug('updating (%s, %s)', incident['sys_id'], incident['short_description']) 
+    else:
+        # setup data for new issue
+        logger.debug('creating (%s, %s)', incident['sys_id'], incident['short_description'])
+        data = { 'fields' : {'project' : {'key' : 'ROYAL'}, 'summary' : incident['short_description'], 'labels' : ['SNow'], \
+                'description': incident['description'], 'issuetype' : {'name' : 'Bug'}, customfield : incident['sys_id'] }}
+        json = json.dumps(data)
+        logger.debug(json)
+        
+        # setup url to create issue and post
+        url = 'https://' + os.environ['JIRA_NAME'] + '.atlassian.net/rest/api/latest/issue/'
+        response = requests.post(url, auth=(os.environ['JIRA_USER'],os.environ['JIRA_TOKEN']), headers={"Content-Type":"application/json"}, json=data, verify=True)
+        
+        # log err and exit if unable to create jira issue
+        if response.status_code != 200:
+            logger.fatal("Unable to create issue(s) on JIRA")
+            exit(1)
+
+    #sys_id = incident['sys_id']
+    #summary = incident['short_description']
+    #description = incident['description']
