b0ac8adb7662fb9be347636f4d0f1d8ccf21faa5
Christopher Towner <christopher.allen.towner@gmail.com>
2019-05-06 12:00:13 -0500

    simplified integration by closing incident after migration to jira

--- s2j.py
+++ s2j.py
@@ -24,6 +24,7 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+# TODO reuse headers
 headers = {"Accept":"application/json", "Content-Type":"application/json"}
 
 # setup url for all incidents assigned to SNOW_USER
@@ -39,51 +40,29 @@ if response.status_code != 200:
     exit(1)
 incidents = json.loads(response.text)['result']
 
-# TODO set maxResults to disable pagination
-# setup jira query for all issues in project assigned to user and incomplete
-url = 'https://' + os.environ['JIRA_NAME'] + '.atlassian.net/rest/api/latest/search?jql=project=' \
-        + os.environ['JIRA_PROJECT'] + '+and+labels=snow+and+statusCategory!=done'
-
-# get all issues
-response = requests.get(url, auth=(os.environ['JIRA_USER'],os.environ['JIRA_TOKEN']), verify=True)
-
-# log err and exit if unable to get jira tickets
-if response.status_code != 200:
-    logger.fatal("Unable to get issues from JIRA")
-    exit(1)
-issues = json.loads(response.text)['issues']
-
-# jira customfield to store snow id
-customfield = os.environ['JIRA_FIELD']
-
-# create issue dictionary based on snow ids
-snowissues = {}
-logger.debug('found %i issue(s)', len(issues))
-for issue in issues:
-    snowissues[issue['fields'][customfield]] = issue
-
 # read-in each incident
 logger.debug('found %i incident(s)', len(incidents))
 for incident in incidents:
-    if incident['sys_id'] in snowissues:
-        logger.debug('updating (%s, %s)', incident['sys_id'], incident['short_description']) 
-    else:
-        # setup data for new issue
-        logger.debug('creating (%s, %s)', incident['sys_id'], incident['short_description'])
-        data = { 'fields' : {'project' : {'key' : 'ROYAL'}, 'summary' : incident['short_description'], 'labels' : ['SNow'], \
-                'description': incident['description'], 'issuetype' : {'name' : 'Bug'}, customfield : incident['sys_id'] }}
-        json = json.dumps(data)
-        logger.debug(json)
-        
-        # setup url to create issue and post
-        url = 'https://' + os.environ['JIRA_NAME'] + '.atlassian.net/rest/api/latest/issue/'
-        response = requests.post(url, auth=(os.environ['JIRA_USER'],os.environ['JIRA_TOKEN']), headers={"Content-Type":"application/json"}, json=data, verify=True)
-        
-        # log err and exit if unable to create jira issue
-        if response.status_code != 200:
-            logger.fatal("Unable to create issue(s) on JIRA")
-            exit(1)
+   # setup data for new issue
+    logger.debug('creating (%s, %s)', incident['sys_id'], incident['short_description'])
+    data = { 'fields' : {'project' : {'key' : 'ROYAL'}, 'summary' : incident['short_description'], 'labels' : ['SNow'], \
+            'description': incident['description'], 'issuetype' : {'name' : 'Bug'} }}
+    
+    # setup url to create issue and post
+    url = 'https://' + os.environ['JIRA_NAME'] + '.atlassian.net/rest/api/latest/issue/'
+    response = requests.post(url, auth=(os.environ['JIRA_USER'],os.environ['JIRA_TOKEN']), headers={"Content-Type":"application/json"}, json=data, verify=True)
+    
+    # log err and exit if unable to create jira issue
+    if response.status_code != 201:
+        logger.fatal("Unable to create issue(s) on JIRA")
+        exit(1)
 
-    #sys_id = incident['sys_id']
-    #summary = incident['short_description']
-    #description = incident['description']
+    url = 'https://' + os.environ['SNOW_NAME'] + '.service-now.com/api/now/table/incident/' \
+            + incident['sys_id']
+    # TODO provide link/id of jira issue
+    data = { 'close_code' : 'Migrated', 'close_notes' : 'Migrated to Atlassian JIRA', 'active' : 'false' , 'state' : 7}
+
+    response = requests.put(url, auth=(os.environ['SNOW_USER'],os.environ['SNOW_PASS']), headers={"Content-Type":"application/json"}, json=data, verify=True)
+    if response.status_code != 200:
+        logger.fatal("Unable to update incident(s) on SNow")
+        exit(1)
